bcd30cc894bc3bc760bc456836ea34820fe9e719
Marti Serra <martiserramolina99@gmail.com>
2025-03-03 13:29:42 +0100

    Updated expry function

--- src/schema-expry/expry/index.ts
+++ src/schema-expry/expry/index.ts
@@ -2,19 +2,19 @@ import { expryInstance } from "@expry/system";
 import { basicOperations, BasicPrototypes } from "@expry/basic";
 import { formityOperations, FormityPrototypes } from "@expry/formity";
 
-import { zodOperations, ZodPrototypes } from "./zod";
 import { componentsOperations, ComponentsPrototypes } from "./components";
+import { zodOperations, ZodPrototypes } from "./zod";
 
 type Prototypes = [
   BasicPrototypes,
-  ZodPrototypes,
   FormityPrototypes,
   ComponentsPrototypes,
+  ZodPrototypes,
 ];
 
 export const expry = expryInstance<Prototypes>(
   basicOperations,
-  zodOperations,
   formityOperations,
   componentsOperations,
+  zodOperations,
 );
